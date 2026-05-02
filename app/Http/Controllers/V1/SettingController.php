9d977bf34d230d6d7983246b9265b7b9f71e5c7a
Insath97 <insath1997.mi@gmail.com>
2026-05-02 20:29:37 +0530

    fix: resolve array to string conversion error in SettingController logging

--- app/Http/Controllers/V1/SettingController.php
+++ app/Http/Controllers/V1/SettingController.php
@@ -87,7 +87,7 @@ class SettingController extends Controller implements HasMiddleware
 
             DB::commit();
 
-            $this->logActivity('UPDATE', 'Setting', "Updated system settings data: {$data}");
+            $this->logActivity('UPDATE', 'Setting', "Updated system settings data: " . json_encode($data));
 
             return response()->json([
                 'status' => 'success',
